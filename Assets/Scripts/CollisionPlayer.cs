8a03de18da7501148e5f099ea2f1e2d65e31f18a
3cecb0fc62049b91 <3cecb0fc62049b91@NS00J002011DE11>
2024-12-09 13:48:40 +1100

    Add project files.

---
+++ Assets/Scripts/CollisionPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerInteract : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{SceneManager.GetActiveScene().name}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -50)
+        {
+            //Debug.Log("Pain");
+            Respawn();
+        }
+    }
+
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Enemy Thouched");
+            Respawn();
+            
+
+            ////Transition to the second one when we want to do damage or something but prob with a game manager 
+            //collision.gameObject.SendMessage("ApplyDamage", 10);
+        }
+
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("You win");
+            Respawn();
+            //GameManager.Win = True
+        }
+    }
+
+    private void Respawn()
+    {
+        SceneManager.LoadScene(3, LoadSceneMode.Single);
+    }
+    public void DeathScreen()
+    {
+       // SceneManager.LoadScene("DeathScreen");
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 2000f;
+    public float jumpspeed = 200f;
+    public GameObject projectilePrefab;
+    public GameObject pauseMenu;
+    public int bulletInGun = 12;
+    public bool noAmmo = false;
+    public Text ammoDisplay;
+
+    bool isGrounded;
+    bool facingRight;
+    public bool ifPaused;
+    private bool ifUnpaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ammoDisplay.text = bulletInGun.ToString("Ammo " + bulletInGun.ToString());
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector2.right * speed * Time.deltaTime);
+            facingRight = true;
+        }
+        // making the player move right
+
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector2.left * speed * Time.deltaTime);
+            facingRight = false;
+        }
+        // making the player move left otherwise
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector2.up * jumpspeed);
+            isGrounded = false;
+        }
+        // making the player move up/jump
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetMouseButtonDown(0) & bulletInGun > 0 & ifPaused == false)
+        {
+            GameObject newProjectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Projectile projectile = newProjectile.GetComponent<Projectile>();
+            if(facingRight)
+            {
+                projectile.direction = Vector3.right;
+            }
+            else
+            {
+                projectile.direction = -Vector3.right;
+            }
+            bulletInGun -= 1;
+        }
+        if( bulletInGun == 0)
+        {
+            //Luke need to integrate into ui if u can thanks   
+            noAmmo = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!ifPaused)
+            {
+                Time.timeScale = 0;
+                ifPaused = true;
+                pauseMenu.SetActive(true);
+
+            }
+
+            else
+            {
+                Time.timeScale = 1;
+                ifPaused = false;
+                pauseMenu.SetActive(false);
+            }
+
+        }
+
+    }
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+        ifPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+            isGrounded = true;
+    }
+}
